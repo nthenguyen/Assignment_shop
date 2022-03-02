62cb4b7806c9034d40a6b8035726202375c3bfea
ntnguyeen <nthenguyeen@gmail.com>
2022-03-02 21:45:56 +0700

    Create search and paging, create product method (crud)

---
+++ RookieShop.Application/Catalog/Products/IPublicProductService.cs
@@ -0,0 +1,16 @@
+﻿using RookieShop.Application.Catalog.Products.Dtos;
+using RookieShop.Application.Catalog.Products.Dtos.Public;
+using RookieShop.Application.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RookieShop.Application.Catalog.Products
+{
+    public interface IPublicProductService
+    {
+        Task<PagedResult<ProductViewModel>> GetAllByCategoryId(GetProductPagedRequest request);
+    }
+}
---
+++ RookieShop.Application/Dtos/PagedRequestBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RookieShop.Application.Dtos
+{
+    public class PagedRequestBase
+    {
+        public int PageIndex { get; set; }
+
+        public int PageSize { get; set; }
+    }
+}
